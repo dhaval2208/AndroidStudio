f5c51a76ce53bef8ad71bd402ed6aa5853e3ba26
dhaval2208 <dhavalbhimani2000@gmail.com>
2022-10-18 20:51:59 -0400

    first commit

---
+++ app/src/main/java/com/dhaval/dhavalbhimani/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dhaval.dhavalbhimani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
